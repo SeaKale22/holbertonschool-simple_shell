74690fabc4650e7101eff86d68a4885b0fc0e21f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-28 09:19:34 -0700

    added some more to header and added fork to s_shell.c

--- s_shell.c
+++ s_shell.c
@@ -12,14 +12,36 @@ int main()
 	int active = 1;
 	pid_t pid;
 	int status;
+	char *command_path;
 
 	while (active)
 	{
+		/* prompt for command */
 		printf("($) ");
-		/* prompts for command */
 		read = getline(&command, &len, stdin);	/*gets input and stores in buffer 'command' */
 		if (read != -1) /* getline successful */
 		{
+			/* removes newline from command */
+			if command[read - 1] == '\n'
+				command[read - 1] = '\0';
+			/* check if command is valid and return path to executable */
+			command_path = valid_command(command);
+			if (command_path =! NULL)
+			{
+				pid = fork();
+				if (pid < 0) /* error in forking */
+				{
+				}
+				else if (pid == 0) /* child process */
+				{
+					/* becomes the command with execve */
+					execve(command_path, /*command line args*/, /*environment*/);
+				}
+				else /* parent process */
+				{
+					wait(status); /*waits for child to run command */
+				}
+			}
 		}
 		else /*getline failed */
 		{
